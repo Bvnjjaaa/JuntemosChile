767df60526e14d8e37d1671a4b0ae0e54f5bea50
Bvnjjaaa <ben.romero@duocuc.cl>
2024-10-02 12:01:41 -0300

    rama benja

---
+++ src/app/api/api-config/api-config.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiConfigService {
+
+  baseUrl = environment.api_url;
+
+  constructor(private http: HttpClient) { }
+
+  // Configurar headers comunes
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'apiKey': environment.apiKey,
+      'Authorization': `Bearer ${environment.apiKey}`
+    });
+  }
+
+  // Manejo de errores
+  private handleError(error: HttpErrorResponse) {
+    console.error('Error ocurrido:', error);
+    return throwError(() => error);
+  }
+
+  // Método GET genérico
+  get<T>(path: string, params?: HttpParams): Observable<HttpResponse<T>> {
+    return this.http.get<T>(`${this.baseUrl}/${path}`, { headers: this.getHeaders(), observe: 'response', params })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+}
